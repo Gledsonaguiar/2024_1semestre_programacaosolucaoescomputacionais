ba2d97014d04d889c5fd103bec0bfe3843b6a933
GledsonAguiar dos Santos <gledsonaguiar1@gmail.com>
2024-03-25 10:07:08 -0300

    aula 25/03/2024

---
+++ Metodos/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function somar( $x , $y){
+    return $x + $y;
+
+
+}
+function imprimirMeuNome(){
+    echo "Gledson Aguiar dos Santos <br>";
+
+}
+
+
+
+imprimirMeuNome();
+$result = somar( 5.1, 10);
+echo "Resultado: " .$result;
+
