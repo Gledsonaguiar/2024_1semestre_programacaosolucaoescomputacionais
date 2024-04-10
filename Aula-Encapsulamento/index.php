2b375d6ea55b8b0da7bbb2fbcf6ba43b7c35457b
Gledsonaguiar <131523406+Gledsonaguiar@users.noreply.github.com>
2024-04-10 01:00:44 -0300

    Aula-Herança

--- Aula-Encapsulamento/index.php
+++ Aula-Encapsulamento/index.php
@@ -1,26 +1,25 @@
 <?php
-include_once("clsBicicleta.php");
-include_once("clsMontainBike.php");
-include_once("clsBicicletaEletrica.php");
+include_once ("clsBicicleta.php");
+include_once ("clsMountainBike.php");
+include_once ("clsBicicletaEletrica.php");
 
-$bicicleta = new Bicicleta("MinhaBike-001","Caloi", "Azul");
-
-echo $bicicleta->getId() . " da marca " . 
-$bicicleta->getMarca() . " e da cor " . 
-$bicicleta->getCor();
+$bicicleta = new Bicicleta("MinhaBike-001", "Caloi", "Azul");
+echo $bicicleta->getId() . " da marca " .
+    $bicicleta->getMarca() . " e da cor " .
+    $bicicleta->getCor();
 
 $bicicleta->andar();
 
-$mountainbike = new MontainBike("Mountain","Sundown","Preta", 1);
-echo "<br>Eu e minha " . $mountainbike->getId() . 
-" da marca ". $mountainbike->getMarca() . " de cor " .
-$mountainbike->getCor() . " na marcha ". $mountainbike->getMarcha();
+$mountainbike = new MountainBike("Mountainbike", "Sundown", "Preta", 1);
+echo "<br>Eu e minha " . $mountainbike->getId() .
+    " da marca " . $mountainbike->getMarca() . " de cor " .
+    $mountainbike->getCor() . " na marcha " . $mountainbike->getMarcha();
 
 $mountainbike->andar();
 $mountainbike->mudarMarcha(3);
 $mountainbike->frear();
 
-$bicicletaEletrica = new BicicletaEletrica("Eletrica","Honda","Vermelha", false, "20%");
+$bicicletaEletrica = new BicicletaEletrica("Eletrica", "Honda", "Vermelha", false, "20%");
 echo "<br>Usando a bicicleta " . $bicicletaEletrica->getId();
 $bicicletaEletrica->ligar();
 $bicicletaEletrica->andar();
@@ -28,4 +27,3 @@ $bicicletaEletrica->frear();
 $bicicletaEletrica->carregar("50%");
 $bicicletaEletrica->andar();
 
-?>
