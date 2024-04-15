c44f7580c3f176f4591658bc3ae241d03a606858
Gledsonaguiar <131523406+Gledsonaguiar@users.noreply.github.com>
2024-04-15 09:16:07 -0300

    Aula Encapsulamento

--- Aula-Encapsulamento/index.php
+++ Aula-Encapsulamento/index.php
@@ -5,23 +5,30 @@ include_once ("clsBicicletaEletrica.php");
 
 $bicicleta = new Bicicleta("Minhabike-001","Caloi","Azul");
 
-echo $bicicleta->getId() . " da marca " .  "Sundown ".
-     $bicicleta->getMarca() . " e da cor " . "Preta";
+echo $bicicleta->getId() . "Minha bike da marca " .  " Sundown ".
+     $bicicleta->getMarca() . " e da cor " . " Preta ";
      $bicicleta->getCor();
-
+     
 $bicicleta->andar();
 
+echo "<hr>";
+
+
+
 $mountainbike = new MountainBike("Mountainbike", "Caloi", "Preta", 1);
-echo "<br>Eu e minha " . $mountainbike->getId() .
-    " da marca " . $mountainbike->getMarca() . " de cor " .
+echo "<br>Eu e minha Bike " . $mountainbike->getId() .
+    " da marca Caloi " . $mountainbike->getMarca() . " de cor Azul " .
     $mountainbike->getCor() . " na marcha " . $mountainbike->getMarcha();
 
 $mountainbike->andar();
 $mountainbike->mudarMarcha(3);
 $mountainbike->frear();
+echo "<hr>";
 
 $bicicletaEletrica = new BicicletaEletrica("Eletrica", "Honda", "Vermelha", false, "20%");
 echo "<br>Usando a bicicleta " . $bicicletaEletrica->getId();
+echo "Da Marca Honda e da cor Verde";
+
 $bicicletaEletrica->ligar();
 $bicicletaEletrica->andar();
 $bicicletaEletrica->frear();
