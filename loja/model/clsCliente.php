4d2f7b2432214eb622c2598ddf979339f6b1c8f4
Gledsonaguiar <131523406+Gledsonaguiar@users.noreply.github.com>
2024-06-23 01:43:16 -0300

    mudança de nome

---
+++ loja/model/clsCliente.php
@@ -0,0 +1,25 @@
+<?php
+
+//include_once ("clsCidade.php");
+
+class Cliente{
+    public $id;
+    public $nome;
+    public $nascimento;
+    public $salario;
+    public $cidade;
+
+    public function __construct($id = NULL, $nome=NULL, $nascimento=NULL, 
+    $salario = 1412.00 , $cidade = NULL ){
+
+        if ($cidade == NULL){
+         $cidade = new Cidade ( 0, "Outra");
+            }
+
+            $this->id = $id;
+            $this->nome = $nome;
+            $this->nascimento = $nascimento;
+            $this->salario = $salario;
+            $this->cidade = $cidade;
+    }
+}
